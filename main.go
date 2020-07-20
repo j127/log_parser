8c7f0699e9231d4537529724508e75c36285c330
J. Cohen <hostelmarketing@gmail.com>
2020-07-20 01:17:32 -0700

    Loop over a slice rather than the map

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 // Usage:
 // go run main.go < data/log.txt
 func main() {
+	var domains []string
 	sum := make(map[string]int)
 	scanner := bufio.NewScanner(os.Stdin)
 
@@ -18,13 +19,22 @@ func main() {
 		fields := strings.Fields(scanner.Text())
 		domain := fields[0]
 		visits, _ := strconv.Atoi(fields[1])
+
+		// putting the domains in a slice keeps the output in order
+		if _, ok := sum[domain]; !ok {
+			domains = append(domains, domain)
+		}
+
 		sum[domain] += visits
 
 	}
 
 	fmt.Printf("%-30s %10s\n", "DOMAIN", "VISITS")
 	fmt.Println(strings.Repeat("-", 41))
-	for domain, visits := range sum {
+
+	// loop over the slice instead of the map
+	for _, domain := range domains {
+		visits := sum[domain]
 		fmt.Printf("%-30s %10d\n", domain, visits)
 	}
 }
